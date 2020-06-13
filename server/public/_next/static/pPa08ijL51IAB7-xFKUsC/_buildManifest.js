25d5ce1727d7aaf0bef2922dc38d955c31b793c1
aykut <aykutfgoktas@gmail.com>
2020-06-13 20:15:43 +0300

    view updated and privacy page added

---
+++ server/public/_next/static/pPa08ijL51IAB7-xFKUsC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/mainpage":["static\u002Fchunks\u002Fa4c92b5b.2a4aebe911251dffaf0e.js",a]}}("static\u002Fchunks\u002F34d17ec4ee0df16c17ab9334e21ae32b86d826d7.a88ac2280a57331a3d96.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
