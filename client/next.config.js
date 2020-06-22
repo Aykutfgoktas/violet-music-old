38d86571dc359c1249e88c6340b0c5f9e845a36c
aykut <aykutfgoktas@gmail.com>
2020-06-23 01:51:05 +0300

    next.config file change

--- client/next.config.js
+++ client/next.config.js
@@ -1,24 +1,3 @@
-/*module.exports = {
-  env: {
-    LOGIN: '/login',
-    GET_POPULAR_SONG: '/getpopularsong',
-    GET_SONG_NOTES: '/getnotes/',
-    CREATE_NOTE: '/createnote',
-  },
-  poweredByHeader: false,
-};*/
-
-/*module.exports = {
-  env: {
-    LOGIN: 'http://localhost:8888/login',
-    GET_POPULAR_SONG: 'http://localhost:8888/getpopularsong',
-    GET_SONG_NOTES: 'http://localhost:8888/getnotes/',
-    CREATE_NOTE: 'http://localhost:8888/createnote',
-    GET_LIST_OF_SONGS: 'http://localhost:8888/getlistofsongs/'
-  },
-  poweredByHeader: false,
-};*/
-
 module.exports = {
   env: {
     LOGIN: 'http://localhost:8888/spotify-api/login',
