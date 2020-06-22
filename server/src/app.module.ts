51469d2b8c8708ad1d878f1cd56dff24b9910552
aykut <aykutfgoktas@gmail.com>
2020-06-23 01:08:31 +0300

    app controller and services deleted

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from "@nestjs/mongoose";
 import { SpotifyApiModule } from './spotify-api/spotify-api.module';
 import { NotesModule } from "./notes/notes.module";
 import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, }), SpotifyApiModule, NotesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
