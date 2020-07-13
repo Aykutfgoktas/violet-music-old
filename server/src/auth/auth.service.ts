eabe19be6322b87eda0f69e68d239fd11e8efd60
Aykut <aykutfgoktas@gmail.com>
2020-07-14 02:12:35 +0300

    auth service fix

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService, private jwtService: JwtService) {}
 
   async validateUser(username: string, password: string): Promise<object> {
     const user = await this.usersService.findOne(username);
-    const isValidPassword = await bcrypt.compare(password, user.password);
-    if (user && isValidPassword) {
-      const { nickname, _id, role } = user;
-      return { nickname, _id, role };
+    if (user) {
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (isValidPassword) {
+        const { nickname, _id, role } = user;
+        return { nickname, _id, role };
+      }
     }
     return null;
   }
